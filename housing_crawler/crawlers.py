b8959a5f15aab543e661ed94f1b5afe82cd2cfd9
Kacper Kubara <kacper@Kacpers-MacBook-Pro.local>
2023-01-11 22:51:45 +0100

    done

--- housing_crawler/crawlers.py
+++ housing_crawler/crawlers.py
@@ -6,16 +6,15 @@ import re
 class OurDomainCrawler():
     def __init__(self, driver):
         self.driver = driver
-        
+        self.result_dict = {}
         self.URL_BASE = "https://southeast-thisisourdomain.securerc.co.uk/onlineleasing/ourdomain-amsterdam-south-east/floorplans.aspx?"
 
-    
     def run(self):
         self.driver.get(self.URL_BASE)
         time.sleep(1)
         element = self.driver.find_element('id', 'divFPRows')
         data_arr = element.find_elements(By.TAG_NAME, 'tr')
-        
+
         print("Iterate over rows")
         for i in range(len(data_arr)):
             prop_name = data_arr[i].find_element(By.XPATH, f"//td[@data-label='Floor Plan'][@data-selenium-id='FPlan_{str(i+1)}']")
@@ -24,7 +23,13 @@ class OurDomainCrawler():
                 "prop_name": prop_name.get_attribute('outerHTML'), 
                 "availability": availability.get_attribute('outerHTML')
                 })
+            if "notified" not in data_dict["availability"].lower():
+                self.result_dict[data_dict["prop_name"]] = data_dict["availability"]
             print(data_dict)
+                
+        if self.result_dict:
+            print("Sending email")
+            self.send_email_to_me(self.result_dict)
 
     def parse_data(self, data_el):
         prop = data_el["prop_name"]
